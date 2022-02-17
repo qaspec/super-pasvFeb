c73211795a101909ac19853d68f3d97233e1bafc
qaspec <75424351+qaspec@users.noreply.github.com>
2022-02-17 14:42:45 +0300

    add first tests (#2)
    add mocha config
    
    Co-authored-by: qaspec <qa.qc.spec@gmail.com>

---
+++ specs/example.spec.js
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+//or const expect = require('chai').expect;
+//import { expect } from 'chai'; all from chai
+describe('Math functions', function (){
+    const a = 4
+    const b = 5
+    const c = 9
+    const d = -1;
+    it('A + B = C', function(){
+        expect(a + b).to.eq(c)
+    })
+    //if want to execute only specific test type only
+    //skip not to run test
+    it.only('A - B = D', function() {
+        expect(a - b).to.eq(d);
+    })
+})
