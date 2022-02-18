afce63ed3a7bd82f96f6dd543d511f6db4a713d4
qaspec <75424351+qaspec@users.noreply.github.com>
2022-02-18 11:10:23 +0300

    add auth tests (#3)
    Co-authored-by: qaspec <qa.qc.spec@gmail.com>

---
+++ specs/auth.spec.js
@@ -0,0 +1,25 @@
+import supertest from 'supertest'
+import {expect} from "chai";
+
+describe('Auth', function () {
+    const request = supertest('https://paysis.herokuapp.com')
+
+    it('successful log in', function () {
+        request
+            .post('/auth')
+            .send({ login: 'adminius', password: 'supers3cret'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(200)
+                expect(res.body.token).not.to.be.undefined
+            })
+    })
+    it('log in with invalid credentials', function () {
+        request
+            .post('/auth')
+            .send({ login: 'invalid', password: 'invalid'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password.')
+            })
+    })
+})
